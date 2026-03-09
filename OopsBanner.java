160d9ffa9da3a5dadbea99e6f2db5c7a1a978d05
Harikrishna0105 <harikrishnamanthu1111@gmail.com>
2026-03-10 01:01:18 +0530

    feature/UC5 Done

--- OopsBanner.java
+++ OopsBanner.java
@@ -1,11 +1,18 @@
 public class OopsBanner {
     public static void main(String[] args) {
-        System.out.println(String.join(" ","   ****   ","   ****    ","  *******  ","   ***** "));
-        System.out.println(String.join(" ","  **  **  ","  **  **   ","  **    ** "," **    "));
-        System.out.println(String.join(" "," **    ** "," **    **  ","  **    ** ","  **    "));
-        System.out.println(String.join(" "," **    ** "," **    **  ","  *******  ","    *** "));
-        System.out.println(String.join(" "," **    ** "," **    **  ","  **       ","       **"));
-        System.out.println(String.join(" ","  **  **  ","  **  **   ","  **       ","       **"));
-        System.out.println(String.join(" ","   ****   ","   ****    ","  **       ","   ***** "));
+
+        String[] lines = { 
+            String.join(" ","   ****   ","   ****    ","  *******  ","   ***** "),
+            String.join(" ","  **  **  ","  **  **   ","  **    ** "," **    "),
+            String.join(" "," **    ** "," **    **  ","  **    ** ","  **    "),
+            String.join(" "," **    ** "," **    **  ","  *******  ","    *** "),
+            String.join(" "," **    ** "," **    **  ","  **       ","       **"),
+            String.join(" ","  **  **  ","  **  **   ","  **       ","       **"),
+            String.join(" ","   ****   ","   ****    ","  **       ","   ***** ")
+        };
+
+        for (String line : lines) {
+            System.out.println(line); 
+        } 
     }
 }
